639f20b75fd50b29b200ba23f717d671d5816af2
Bazif Abdullah <bazifabdullah@gmail.com>
2025-11-08 07:03:40 +0500

    Refactor: remove Step1 component to initial in better way

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import Step1 from "./components/step1"
 
 function App() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-magnolia">
-      <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-        <Step1 />
-      </div>
-    </main>
+    <p>Hello</p>
   )
 }
 
